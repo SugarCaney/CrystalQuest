69176080cd26d2a12653525bd1b46db7b840dcb7
SugarCaney <mrsugarcaney@gmail.com>
2019-03-30 16:39:52 +0100

    Fixed wand breaking blocks.

--- src/nl/sugcube/crystalquest/game/Protection.java
+++ src/nl/sugcube/crystalquest/game/Protection.java
@@ -225,5 +225,6 @@ public class Protection implements Listener {
                             plugin.protection.pos2.getZ()
                     )));
         }
+        e.setCancelled(true);
     }
 }
